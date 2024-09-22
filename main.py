ea4184e53d1ae5bc96ea6c93903065f188be4588
Covald <rezin.p@gmail.com>
2024-09-23 02:05:04 +0700

    change some logic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import functools
 import os
 import random
+import typing
 from configparser import ConfigParser
 
 import pyrogram
@@ -16,9 +18,10 @@ config = ConfigParser()
 # parse existing file
 config.read('config.ini')
 
-proc_chance = config.getint("base", "proc_chance")
+vova_proc_chance = config.getint("base", "vova_proc_chance")
+all_proc_chance = config.getint("base", "all_proc_chance")
 # admin_list = config["admin_list"]
-print(proc_chance)
+
 app = Client(
     "my_bot",
     # "my_account",
@@ -27,6 +30,8 @@ app = Client(
 )
 
 svetloe = -1001947538282
+
+TEST_CHAT = -4533866272
 
 andecdot_list = ["https://www.youtube.com/watch?v=k5vSmYlvMb0&ab_channel=ATVPlus",
                  "https://www.youtube.com/watch?v=jrHXEYWXI04&ab_channel=%D0%9C%D0%B8%D1%85%D0%B0",
@@ -37,35 +42,50 @@ andecdot_list = ["https://www.youtube.com/watch?v=k5vSmYlvMb0&ab_channel=ATVPlus
                  ]
 
 
+@app.on_message(filters.command("get_chat") & filters.user("@Covald"))
+async def get_chat(client: pyrogram.Client, message: pyrogram.types.Message):
+    await client.send_message(926836636, f"{message.chat.username or message.chat.title} - {message.chat.id}")
+
+
+# @app.on_message(filters.chat(TEST_CHAT)("/who@vkvante_bot"))
+# async def whoi(client: pyrogram.Client, message: pyrogram.types.Message):
+#     await message.reply("Й")
+
+
+@app.on_message(filters.chat(svetloe) & filters.user("@torchCat"))
+async def vova_answer(client: pyrogram.Client, message: pyrogram.types.Message):
+    if random.randrange(0, 100, 1) > vova_proc_chance:
+        return
+    await message.reply("Хр хр")
+
+
 @app.on_message(filters.chat([svetloe, ]) & filters.text)
-async def pizdez_vlad(client, message):
+async def pizdez_vlad(client, message: pyrogram.types.Message):
     # Added 10 percent prb of replying
+    global all_proc_chance
+    if random.randrange(0, 100, 1) > all_proc_chance:
+        return
     message_len = len(message.text.split())
-    first_word = message.text.split()[0].lower().strip()
-    last_word = message.text.split()[-1].lower().strip()
-    global proc_chance
-    if random.randrange(0, 100, 1) <= proc_chance:
+    first_word = message.text.split()[0].lower().strip().replace(".", "").replace(",", "")
+    last_word = message.text.split()[-1].lower().strip().replace(".", "").replace(",", "")
 
-        if "пиздец" in first_word and message_len == 1:
-            await message.reply("Влад")
-        elif "да" in last_word:
-            await message.reply("Пизда.")
-        elif "нет" in last_word:
-            await message.reply("Ты знаешь чей ответ.")
-        elif "пизда" in first_word and message_len == 1:
-            await message.reply("Пиздец")
-        elif "влад" == first_word and message_len == 1:
-            await message.reply("да")
-
-    if "блядство" in first_word and message_len == 1:
+    if "пиздец" in first_word and message_len == 1:
+        await message.reply("Влад")
+    elif "да" == last_word[-2:-1].lower():
+        await message.reply("Пизда.")
+    elif "нет" == last_word.lower():
+        await message.reply("Ты знаешь чей ответ.")
+    elif "пизда" == first_word[-5:-1].lower():
+        await message.reply("Да.")
+    elif "блядство" in last_word.lower():
         await message.reply("Полностью поддерживаю!")
-    if "вот такая есть" in message.text.lower().strip() and message_len == 3:
+    if "вот такая есть" in message.text.lower() and message_len == 3:
         await message.reply("Ахвахвххва, ебанько!")
-    if "хочу анекдот" in message.text.lower().strip() and message_len == 2:
+    if "анекдот" in last_word.lower():
         await message.reply("".join(random.choices(andecdot_list)))
 
 
-@app.on_message(filters.command(["chat_list", ]) & filters.text & filters.private)
+@app.on_message(filters.command(["chat_list", ]) & filters.private & filters.user("@Covald"))
 async def get_chats(client: pyrogram.Client, message):
     _chats = []
     async for i in client.get_dialogs():
@@ -73,21 +93,49 @@ async def get_chats(client: pyrogram.Client, message):
     message.reply("\n".join(map(lambda x: str(x[0]) + " - " + str(x[1]), _chats)))
 
 
-@app.on_message(filters.command(["change_proc_chance", ]) & filters.text & filters.private)
-async def get_chats(client: pyrogram.Client, message: pyrogram.types.Message):
+@app.on_message(
+    filters.command(["change_proc_chance_for_vova", ]) & filters.text & filters.private & filters.user("@Covald"))
+async def change_chanche_for_vova(client: pyrogram.Client, message: pyrogram.types.Message):
     _message = message.text.split()
+
+    if len(_message) == 1:
+        await message.reply(vova_proc_chance)
+        return
+
     global config
-    print(_message)
-    if "change_proc_chance" in _message[0]:
-        try:
-            value = str(int(_message[1]))
-        except Exception as err:
-            await message.reply("Pls, sobaka ti sutulaya, use int parameter.")
-            return
-        config.set("base", "proc_chance", value)
+    try:
+        value = str(int(_message[1]))
+    except Exception as err:
+        await message.reply("Pls, sobaka ti sutulaya, use int parameter.")
+        return
+    config.set("base", "vova_proc_chance", value)
 
     global proc_chance
-    proc_chance = config.getint("base", "proc_chance")
+    proc_chance = config.getint("base", "vova_proc_chance")
+    await message.reply(f"Okay, now chance for answer is {proc_chance}%.")
+    with open("config.ini", "w") as fp:
+        config.write(fp)
+
+
+@app.on_message(
+    filters.command(["change_proc_chance_for_all", ]) & filters.text & filters.private & filters.user("@Covald"))
+async def get_chats(client: pyrogram.Client, message: pyrogram.types.Message):
+    _message = message.text.split()
+
+    if len(_message) == 1:
+        await message.reply(all_proc_chance)
+        return
+
+    global config
+    try:
+        value = str(int(_message[1]))
+    except Exception as err:
+        await message.reply("Pls, sobaka ti sutulaya, use int parameter.")
+        return
+    config.set("base", "all_proc_chance", value)
+
+    global proc_chance
+    proc_chance = config.getint("base", "all_proc_chance")
     await message.reply(f"Okay, now chance for answer is {proc_chance}%.")
     with open("config.ini", "w") as fp:
         config.write(fp)
